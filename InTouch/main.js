112b41a732d885ad2f67aaac509925c563d5f7f5
Sebmins <sebmin@btinternet.com>
2020-03-12 14:16:46 +0000

    Added on website

--- InTouch/main.js
+++ InTouch/main.js
@@ -1,12 +1,12 @@
 var clickable = true;
 var betAmount; // £10 bet
-var headString = "You bet £";
+var headString;
+var gameover = false;
 
 cvs.addEventListener('mousedown', function(e){
 console.log(clickable);
     // Get canvas position on page
     var canvasBounds = cvs.getBoundingClientRect();
-
     // Turn click coordinates into canvas coordinates 1
     var localX = e.pageX - canvasBounds.left;
     var localY = e.pageY - canvasBounds.top;
@@ -16,6 +16,12 @@ console.log(clickable);
             clickable = false;
             genNumber();
         }
+    }    
+});
+
+cvs.addEventListener('mousedown', function(e){
+    if(gameover===true){
+        window.location.reload(true);
     }
 });
 
@@ -26,10 +32,15 @@ function main(){
     betAmount = 10;
 }
 
+function gameEnd(){
+    console.log("gameover"+gameover);
+    gameover = true;
+}
+
 function win(){
+    setTimeout(gameEnd, 5000);
     angle=angle+1000000;
     headingText("YOU WIN £"+betAmount*getWinningInt());
     setTimeout(rotateAntiCL, 1000);
 }
-
 main();
